ea1c308054c4e7e32af1d91663ad487244552f57
Mohnish Kadakia <monukadakia@gmail.com>
2017-04-29 14:03:06 -0700

    added performance hash map for grades

--- requirements.py
+++ requirements.py
@@ -1,5 +1,6 @@
 class Classes(object):
 	classes = None
+	grade = None
 
 	def __init__(self):
 		self.prereqs()
@@ -36,5 +37,20 @@ class Classes(object):
 			"CS190I" : "CS146",
 			}
 
-b = Classes()
-b.prereqs()
+	def grades(self):
+		self.grade = {
+			"A+" : "4.0",
+			"A" : "4.0",
+			"A-" : "3.7",
+			"B+" : "3.3",
+			"B" : "3.0",
+			"B-" : "2.7",
+			"C+" : "2.3",
+			"C" : "2.0",
+			"C-" : "1.7",
+			"D+" : "1.3",
+			"D" : "1.0",
+			"D-" : "0.7",
+			"F" : "0",
+			}
+
