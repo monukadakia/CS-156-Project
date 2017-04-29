4e654c81e490dd905fe3f28427fc0f17faae2267
Swonboyyash <parikhyash366@gmail.com>
2017-04-29 14:43:03 -0700

    pyc changes

--- requirements.py
+++ requirements.py
@@ -36,7 +36,7 @@ class Classes(object):
 			"CS190" : "CS146",
 			"CS190I" : "CS146",
 			}
-			
+
 	def grades(self):
 		self.grade = {
 			"A+" : "4.0",
@@ -53,5 +53,3 @@ class Classes(object):
 			"D-" : "0.7",
 			"F" : "0",
 			}
-
-
