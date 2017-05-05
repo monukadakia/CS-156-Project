ceb330c888ddcbd57ffd62028850924a95c032dc
Mohnish Kadakia <monukadakia@gmail.com>
2017-05-05 12:47:52 -0700

    merging conflicts

--- agents.py
+++ agents.py
@@ -87,9 +87,9 @@ class Agent(object):
                     self.ratio = (self.ratio[0]-1,self.ratio[1]-1)
                 elif(self.CSGPA < 3.3 and self.ratio[0] > 1):
                     self.ratio = (self.ratio[0]-1,self.ratio[1])
-                elif(self.CSGPA < 3.3 and self.ratio[1] > 1):
+                elif(self.GEGPA < 3.3 and self.ratio[1] > 1):
                     self.ratio = (self.ratio[0],self.ratio[1]-1)
-                else:
+                elif():
                     print "Your GPA is too low. Please take a semester off."
             if(self.ratio[0] == len(self.classAvailable)):
                 self.final_schedule = self.classAvailable
